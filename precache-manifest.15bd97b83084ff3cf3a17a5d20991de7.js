64ef288abc5b5b964ded98be1c9ea7005a6679b0
Kevin Sommer <kevin@rwth.de>
2020-10-24 23:30:13 +0200

    Updates

---
+++ precache-manifest.15bd97b83084ff3cf3a17a5d20991de7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423c1b95f943e3ad3952399bed12e363",
+    "url": "/KevinSommer96/strava-heatmap-gif-frontend/index.html"
+  },
+  {
+    "revision": "4b39f068fb4a5cd36a69",
+    "url": "/KevinSommer96/strava-heatmap-gif-frontend/static/css/2.0a5b19c0.chunk.css"
+  },
+  {
+    "revision": "65414f89e4b4bfbf719b",
+    "url": "/KevinSommer96/strava-heatmap-gif-frontend/static/css/main.828ff272.chunk.css"
+  },
+  {
+    "revision": "4b39f068fb4a5cd36a69",
+    "url": "/KevinSommer96/strava-heatmap-gif-frontend/static/js/2.5384f7eb.chunk.js"
+  },
+  {
+    "revision": "ee9d6ff43844efbe4ddeab3c8d2b69b8",
+    "url": "/KevinSommer96/strava-heatmap-gif-frontend/static/js/2.5384f7eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65414f89e4b4bfbf719b",
+    "url": "/KevinSommer96/strava-heatmap-gif-frontend/static/js/main.4b7a8850.chunk.js"
+  },
+  {
+    "revision": "2ba9b341cc3a56cbb5fd",
+    "url": "/KevinSommer96/strava-heatmap-gif-frontend/static/js/runtime-main.ba4e167a.js"
+  },
+  {
+    "revision": "207e1bd0142adfc5f29db9bef8cbc17d",
+    "url": "/KevinSommer96/strava-heatmap-gif-frontend/static/media/gif.207e1bd0.png"
+  },
+  {
+    "revision": "f05de17ab2ad4431ea51bb8b826ba18b",
+    "url": "/KevinSommer96/strava-heatmap-gif-frontend/static/media/map.f05de17a.png"
+  }
+]);
